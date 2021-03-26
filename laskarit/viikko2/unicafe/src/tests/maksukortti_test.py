a39b274e8aaba03f3999c760107bc920dcc270fc
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2021-03-26 21:13:35 +0200

    kassapaate_testi luotu

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,26 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertNotEqual(str(self.maksukortti), "saldo 0.1")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertNotEqual(str(self.maksukortti), "saldo 0.2")
+
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertNotEqual(str(self.maksukortti), "saldo 0.05")
+
+    def test_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(20)
+        self.assertNotEqual(str(self.maksukortti), "saldo 0.2")
+
+    def test_rahat_riittavat(self):
+        self.maksukortti.ota_rahaa(10)
+        self.assertNotEqual(self.maksukortti, True)
+
+    def test_rahat_eivat_riita(self):
+        self.maksukortti.ota_rahaa(30)
+        self.assertNotEqual(self.maksukortti, False)

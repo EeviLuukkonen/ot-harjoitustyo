44f2e18beb91b7c90db9d0c4a64aa2504a60b226
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2021-04-07 10:09:52 +0300

    aiheen vaihto

--- src/index.py
+++ src/index.py
@@ -1,32 +1,9 @@
-import pygame
-from clock import Clock
-from shapes import Shapes
-from gameloop import Gameloop
-from renderer import Renderer
+from pygame import Pygame
+from display import Display
 
 def main():
-    display_height = 700
-    display_width = 800
-    height = 600
-    width = 300
-    top_left_x = (display_width - width) // 2
-    top_left_y = display_height - height   
-
-    display = pygame.display.set_mode((display_width, display_height))
-    pygame.display.set_caption("Tetris")
-
-    clock = Clock()
-    renderer = Renderer(display, height, width, top_left_x, top_left_y)
-    game_loop = Gameloop(clock, renderer)
-
-
-    piece_shape = Shapes()
-    #piece_shape.get_shape
-
+    width = 800
+    height = 500
+    pygame.display.set_mode(width, height)
+    pygame.display.set_caption("Hangman")
     pygame.init()
-    game_loop.start()
-
-
-
-if __name__ == "__main__":
-    main()
--- src/piece.py
+++
@@ -1,9 +0,0 @@
-from shapes import Shapes
-
-class Piece():
-    def __init__(self, column, row, shape):
-        self.x = column
-        self.y = row
-        self.shape = shape
-        self.color = Shapes.colours[Shapes.shapes.index(shape)]
-        self.rotation = 0

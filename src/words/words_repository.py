1a29a7e2aafcee1f146c7bb1c21e9312e5996fe4
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2021-05-10 11:55:15 +0300

    projekti repon juureen

--- src/words/words_repository.py
+++ src/words/words_repository.py
@@ -4,26 +4,26 @@ class WordRepository:
     def __init__(self, file_path):
         self.file_path = file_path
     def easy_word(self):
-        medium_words = []
-        words = self.find_all()
-        for i in words:
-            if 5 < len(i) < 10:
-                medium_words.append(i)
-        return random.choice(medium_words)
-    def medium_word(self):
-        hard_words = []
-        words = self.find_all()
-        for i in words:
-            if 9 < len(i) < 15:
-                hard_words.append(i)
-        return random.choice(hard_words)
-    def hard_word(self):
         easy_words = []
         words = self.find_all()
         for i in words:
             if  3 < len(i) < 6:
                 easy_words.append(i)
         return random.choice(easy_words)
+    def medium_word(self):
+        medium_words = []
+        words = self.find_all()
+        for i in words:
+            if 5 < len(i) < 10:
+                medium_words.append(i)
+        return random.choice(medium_words)
+    def hard_word(self):
+        hard_words = []
+        words = self.find_all()
+        for i in words:
+            if 9 < len(i) < 15:
+                hard_words.append(i)
+        return random.choice(hard_words)
     def find_all(self):
         return self.read()
     def read(self):

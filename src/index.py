5e80147fefdd97567af3ffe471f23002511cc4d0
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2021-05-13 12:00:34 +0300

    build now working

--- src/index.py
+++ src/index.py
@@ -3,7 +3,6 @@ from images.load_images import load_images
 from ui.display import Display
 from gamelogic.menu import Menu
 from gamelogic.event_queue import EventQueue
-from initialize_database import initialize_database
 
 def main():
     pygame.init()
@@ -11,12 +10,10 @@ def main():
     height = 700
     win = pygame.display.set_mode((width, height))
     images = load_images()
-    initialize_database()
     event_queue = EventQueue()
     display = Display(win, width, height, images)
     menu = Menu(display, event_queue, width, height)
     menu.menu()
-
 
 if __name__ == "__main__":
     main()

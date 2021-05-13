232516263175fbaf0b8429df91fb0970d709f42b
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2021-05-13 11:23:10 +0300

    index fix

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,4 +1,3 @@
-import os
 import sqlite3
 from config import SCORES_FILEPATH
 
--- src/index.py
+++ src/index.py
@@ -3,17 +3,20 @@ from images.load_images import load_images
 from ui.display import Display
 from gamelogic.menu import Menu
 from gamelogic.event_queue import EventQueue
+from initialize_database import initialize_database
 
 def main():
     pygame.init()
     width = 600
     height = 700
     win = pygame.display.set_mode((width, height))
+    initialize_database()
     images = load_images()
     event_queue = EventQueue()
     display = Display(win, width, height, images)
     menu = Menu(display, event_queue, width, height)
     menu.menu()
+
 
 if __name__ == "__main__":
     main()
